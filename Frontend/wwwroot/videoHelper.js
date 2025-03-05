7a0ab51230f4ab48e6b04d970439d2c8986b33dd
joakimolofsson-iths <joakim.olofsson@iths.se>
2025-03-05 16:53:00 +0100

    Removed video. Added a new launch profile

--- Frontend/wwwroot/videoHelper.js
+++
@@ -1,8 +0,0 @@
-﻿window.playVideo = (videoId) => {
-    setTimeout(() => {
-        let video = document.getElementById(videoId);
-        if (video) {
-            video.play().catch(error => console.error("Autoplay blocked:", error));
-        }
-    }, 500); // 500ms delay to let Blazor finish rendering
-};
