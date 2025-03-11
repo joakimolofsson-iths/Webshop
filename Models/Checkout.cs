429695a48c7c03558d6ed4a9c2c155fdba3122de
joakimolofsson-iths <joakim.olofsson@iths.se>
2025-03-11 20:15:00 +0100

    Added a form in Cart and a new page: Confirmation

---
+++ Models/Checkout.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class Checkout
+	{
+		[Required(ErrorMessage = "First name is required")]
+		public string? FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last name is required")]
+		public string? LastName { get; set; }
+
+		[Required(ErrorMessage = "E-mail address is required")]
+		[EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+		public string? Email { get; set; }
+
+		[Required(ErrorMessage = "Phone number is required")]
+		public string? Phone { get; set; }
+
+		[Required(ErrorMessage = "City is required")]
+		public string? City { get; set; }
+
+		[Required(ErrorMessage = "Street address is required")]
+		public string? Street { get; set; }
+
+		[Required(ErrorMessage = "ZIP code is required")]
+		public string? ZipCode { get; set; }
+	}
+}
